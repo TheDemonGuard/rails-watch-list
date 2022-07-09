e71aeca36aa82dee2b852c1d8674d5c1e89107c9
Henry Ishiyama <henryishiyama@gmail.com>
2022-07-09 13:21:32 +0900

    Implemented upto Seed

---
+++ app/models/list.rb
@@ -0,0 +1,6 @@
+class List < ApplicationRecord
+  validates :name, presence: true, uniqueness: true # uniqueness: true, allow_blank: false, presence: { strict: true }
+  has_many :bookmarks, dependent: :destroy
+  # has_many :movies, through: bookmarks #, dependent: :destroy
+  has_many :movies, through: :bookmarks # , dependent: :destroy
+end
